f28fd1e9dfac7d6c3002a6482efd8e85de435a43
mhsn06 <mhsn06@gmail.com>
2019-01-13 09:40:07 +0600

    table bug fix

--- src/App.js
+++ src/App.js
@@ -1,28 +1,47 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import Table from './Table';
+
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	
+	removeCharacter = index => {
+		const { characters } = this.state;
+
+		this.setState({
+			characters: characters.filter((character, i) => { 
+				return i !== index;
+			})
+		});
+	}
+    state = {
+			characters: [
+				{
+                'name': 'Charlie',
+                'job': 'Janitor'
+				},
+				{
+					'name': 'Mac',
+					'job': 'Bouncer'
+				},
+				{
+					'name': 'Dee',
+					'job': 'Aspring actress'
+				},
+				{
+					'name': 'Dennis',
+					'job': 'Bartender'
+				}
+			]        
+		};
+	render() {
+        return (
+             <div className="container">
+				<Table characterData={this.state.characters}
+				removeCharacter={this.removeCharacter} 
+				/>
+			</div>
+        );
+    }
 }
 
 export default App;
