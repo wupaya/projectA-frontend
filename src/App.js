e6f9d47b23c02a8a9ba4307295cc8d3e00b07183
mhsn06 <mhsn06@gmail.com>
2019-01-30 22:09:53 +0600

    added jumbletrol

--- src/App.js
+++ src/App.js
@@ -56,10 +56,19 @@ class App extends Component {
 
         return (
         <div>
-            <ul>{data}</ul>
-            <Login value="some value"/>
-            <InstitutePageManagement />
+        <div className="jumbotron">
+            <h1>Welcome to Test Project</h1> 
+            <p>This project is for demonestration purpose only.</p> 
         </div>
+<Login value="some value"/>
+        </div>
+          // <p>This is some text.</p> 
+          // <p>This is another text.</p>
+          // <div>
+            // <ul>{data}</ul>
+            
+            // <InstitutePageManagement />
+          // </div>
         );
     }
 }
@@ -74,17 +83,45 @@ class Login extends Component{
     }
     render(){
         return (
-        <form action="#">
-        <input 
-        type="text" 
-        value={this.state.value}
-        onClick={() => this.setState({value: 'X'})}
-        /><br />
-        <input 
-        type="text" 
-        value={this.state.value}
-        />
-        </form>
+            <form action="#">
+  <div class="form-row align-items-center">
+    <div class="col-auto">
+      <label class="sr-only" for="inlineFormInput">Name</label>
+      <input type="text" class="form-control mb-2" id="inlineFormInput" placeholder="Jane Doe" />
+    </div>
+    <div class="col-auto">
+      <label class="sr-only" for="inlineFormInputGroup">Username</label>
+      <div class="input-group mb-2">
+        <div class="input-group-prepend">
+          <div class="input-group-text">**</div>
+        </div>
+        <input type="password" class="form-control" id="inlineFormInputGroup" placeholder="Password" />
+      </div>
+    </div>
+    <div class="col-auto">
+      <div class="form-check mb-2">
+        <input class="form-check-input" type="checkbox" id="autoSizingCheck" />
+        <label class="form-check-label" for="autoSizingCheck">
+          Remember me
+        </label>
+      </div>
+    </div>
+    <div class="col-auto">
+      <button type="submit" class="btn btn-primary mb-2">Submit</button>
+    </div>
+  </div>
+</form>
+        // <form action="#">
+        // <input 
+        // type="text" 
+        // value={this.state.value}
+        // onClick={() => this.setState({value: 'X'})}
+        // /><br />
+        // <input 
+        // type="text" 
+        // value={this.state.value}
+        // />
+        // </form>
         );
     }
 }
