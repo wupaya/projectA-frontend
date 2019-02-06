64c0a49bd2254c9dcf1f5b899162b9e17342ac1c
Riyadh <riyadhhazzaz1122@gmail.com>
2019-02-06 22:39:51 +0600

    new table added

--- src/App.js
+++ src/App.js
@@ -62,6 +62,7 @@ class App extends Component {
         </div>
 <Login value="some value"/>
 <HelloComponent />
+<Table />
 <Hello2Component />
 <Hello3Component />
 <Hello4Component user="Riyadh" />
@@ -203,5 +204,37 @@ class Hello4Component extends Component{
 			<p>Hello{this.props.user}</p>
 			)
      }
+}
+class Table extends Component {
+    render() {
+        return (
+            <table>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Job</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Charlie</td>
+                        <td>Janitor</td>
+                    </tr>
+                    <tr>
+                        <td>Mac</td>
+                        <td>Bouncer</td>
+                    </tr>
+                    <tr>
+                        <td>Dee</td>
+                        <td>Aspiring actress</td>
+                    </tr>
+                    <tr>
+                        <td>Dennis</td>
+                        <td>Bartender</td>
+                    </tr>
+                </tbody>
+            </table>
+        );
+    }
 }
 export default App;
