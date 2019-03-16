b1e4d039019d9091423f37d0a929c4c94b227226
Rishat05 <rishatrifa56@gmail.com>
2019-03-16 23:32:21 +0600

    component add koresi

--- src/rishat.js
+++ src/rishat.js
@@ -1,18 +1,17 @@
-import React, {Component} from 'react';
 
-export class Hassan extends Component{
-	render(){
-		return(
-		<h1>Hassan </h1>
-		)
-	}
- }
-export class mHassa2 extends Component{
-	render(){
-		return(
-		<h1>Hassan bla </h1>
-		)
-	}
- }
- // export default Hassan;
+import React,{Component} from 'react';
 
+export class rishat1 extends Component{
+	render(){
+		return (
+		  <h1>Rishat </h1>
+		  )
+	}
+}
+ export class rishat2 extends Component{
+	 render(){
+		 return (
+		 <h1> Rishat Raj </h1>
+		 )
+	 }
+ }
