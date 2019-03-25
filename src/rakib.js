6aceee2c6b5920a5b3acece8c3d755c2a713db51
rakibjabed <rakibjabed58@gmail.com>
2019-03-25 23:32:15 +0600

    amar component define koresi

--- src/rakib.js
+++ src/rakib.js
@@ -1,16 +1,16 @@
 import React, {Component} from 'react';
 
-export class Hassan extends Component{
+export class Rakib1 extends Component{
 	render(){
 		return(
-		<h1>Hassan </h1>
+		<h1>Rakib1</h1>
 		)
 	}
  }
-export class mHassa2 extends Component{
+export class Rakib2 extends Component{
 	render(){
 		return(
-		<h1>Hassan bla </h1>
+		<h1>Rakib bla </h1>
 		)
 	}
  }
