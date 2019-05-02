ca42a28042d6df94488d30de12abbdb6225150b0
Rakib <rakibjabed58@gmail.com>
2019-05-02 22:46:47 +0600

    new component

--- src/rakib.js
+++ src/rakib.js
@@ -75,6 +75,25 @@ export class Rakib4 extends Component{
         )
     }
   }
- 
+  export class StateExample extends Component{
+	  constructor(props) {
+		  super(props);
+		  this.state = { text: "bat" };
+	  }
+	  
+	  ban() {
+		  this.setState ({ text: "ban" });
+	  }
+	  
+	  render () {
+		  return (
+		  <div>
+		  <button onClick={() => this.ban()}>ban</button>
+		  <p>{this.state.text}</p>
+		  </div>
+		  );
+	  }
+  }
+	
 // export default Hassan;
 
