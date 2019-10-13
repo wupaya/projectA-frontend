37e2da64df8a1c91c15a930c74f740cbbcb10139
Riyadh <riyadhhazzaz1122@gmail.com>
2019-10-13 23:49:18 +0600

    new data show

--- src/institute/ins_task_attendence.js
+++ src/institute/ins_task_attendence.js
@@ -31,6 +31,32 @@ class TakeAttendence extends Component{
             <div>
                 <BreadcrumbsItem to="/private/service/eduman/:id">Manage Public Page</BreadcrumbsItem>
                 <p>You will take attendence here</p>
+				<table class="table">
+					<thead>
+					  <tr>
+						<th>Firstname</th>
+						<th>Lastname</th>
+						<th>Email</th>
+					  </tr>
+					</thead>
+					<tbody>
+					  <tr>
+						<td>John</td>
+						<td>Doe</td>
+						<td>john@example.com</td>
+					  </tr>
+					  <tr>
+						<td>Mary</td>
+						<td>Moe</td>
+						<td>mary@example.com</td>
+					  </tr>
+					  <tr>
+						<td>July</td>
+						<td>Dooley</td>
+						<td>july@example.com</td>
+					  </tr>
+					</tbody>
+				</table>
                
             </div>
         );
