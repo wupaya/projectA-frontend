57f50c28ac3d1ee78abfdc0c9f3cf4400c0b4afa
maidul <maidul@semanticslablab.net>
2019-10-16 13:28:45 +0600

    adding feature to page

--- src/institute/ins_page.js
+++ src/institute/ins_page.js
@@ -1,60 +1,41 @@
-import React, {Component} from 'react';
+import React, {Component, Suspense} from 'react';
 import $ from 'jquery';
 import Cookies from 'js-cookie';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link, NavLink } from "react-router-dom";
 import { withRouter} from "react-router-dom";
-import {NavBar, Footer} from '../commons'
+import {NavBar, Footer} from '../commons';
+import { Breadcrumbs, BreadcrumbsItem } from "react-breadcrumbs-dynamic";
 
 class Page1 extends Component{
-    state ={
-      page:{
-        nuid:1231,
-        sid:"begum_rokey_univ",
-        title: "Department of Computer Science and Engineering",
-        subtitle: "One of the Departments at Begum Rokeya University, Rangpur",
-        contact:"",
-        short_description:"",
-        quick_overview:"It is founded in 2008. At present 250 students are enrolled in this discipline. There are 10 world class faculty memebers.",
-        lastest_events:[
-          {title:"News: CSE BRUR started using IMS system.",details_link:"#"},
-          {title:"Event: Inter batch programming contest on sunday, 9th oct.",details_link:"#"},
-          {title:"Circular: Admission is open",details_link:"#"},
-          {title:"Notice: New Policy for Scholarship.",details_link:"#"}
-        ]
-      
 
-      }
-    }
     onLogOutHangle(){
       Cookies.remove("token");
     }
 
     render(){
-        let {page} = this.state;
+        //let {page} = this.state;
         //{this.props.match.params.id}
+        let {location} = this.props;
+        let params = new URLSearchParams(location.search);
+        var action = params.get("action")
+        var viewid = params.get("viewid")
+
+        var View = <PageBase />;
+
+        if(action=="page"){
+            if(viewid==null){
+              return <p>Something went wrong.</p>
+            }
+            View = React.lazy(() => import('./page/'+ viewid));
+        }
         return(
         <div className="container">
             <NavBar onLogOut={this.onLogOutHangle.bind(this)} />
 
-            <div class="row">
-              <div class="col-lg-2">
-                  <svg class="bd-placeholder-img img-thumbnail" width="200" height="200" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="A generic square placeholder image with a white border around it, making it resemble a photograph taken with an old instant camera: 200x200"><rect width="100%" height="100%" fill="#868e96"></rect><text x="50%" y="50%" fill="#dee2e6" dy=".3em">200x200</text></svg>
-                  <SideBar />
-                </div>
-                <div class="col-lg-10">
-                <h2>{page.title}</h2>
-                <p>{page.subtitle}</p>
-                <hr />
-                <h5>Quick Overview</h5>
-                <p>{page.quick_overview}</p>
-                <hr />
-                <h5>Latest Events</h5>
-                <ul>
-                  {page.lastest_events.map((key, index)=>{return <li>{key["title"]}</li>})}
-                </ul>
-              </div>
+              <Suspense fallback={<div>Loading...</div>}>
+                  <View match={this.props.match}/>
+              </Suspense>
 
-            </div>
             <div className="row">
                 <Footer />
             </div>
@@ -65,84 +46,77 @@ class Page1 extends Component{
 
 const Page = withRouter(Page1);
 export default Page;
-class SideBar extends Component{
 
-    render(){
-        return(
-                          <ul class="nav flex-column">
-  <li class="nav-item">
-    <a class="nav-link active" href="#">Home</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="#">Contact</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="#">Notices</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="#">Events</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="#">Gallery</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="#">Allumni</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Suppliers</a>
-  </li>
-</ul>
-        );
+class PageBase extends Component{
+  state ={
+    page:{
+      nuid:1231,
+      sid:"begum_rokey_univ",
+      title: "Department of Computer Science and Engineering",
+      subtitle: "One of the Departments at Begum Rokeya University, Rangpur",
+      contact:"",
+      short_description:"",
+      quick_overview:"It is founded in 2008. At present 250 students are enrolled in this discipline. There are 10 world class faculty memebers.",
+      lastest_events:[
+        {title:"News: CSE BRUR started using IMS system.",details_link:"#"},
+        {title:"Event: Inter batch programming contest on sunday, 9th oct.",details_link:"#"},
+        {title:"Circular: Admission is open",details_link:"#"},
+        {title:"Notice: New Policy for Scholarship.",details_link:"#"}
+      ],
+    sections:[
+      {nuid: 1,comid:"contact", description:"Contact us"},
+      {nuid: 2,comid:"gallery", description:"Visit galleries"},
+      {nuid: 3,comid:"events", description:"Show events"},
+      {nuid: 4,comid:"notices", description:"Show recent notices"},
+      {nuid: 5,comid:"services", description:"Show services for parents"},
+      {nuid: 6,comid:"alumni", description:"Alumni"},
+      {nuid: 7,comid:"career", description:"Career offers"}
+    ]
     }
-}
-
-class Search extends Component{
-
-    baz(e){
-         e.preventDefault();
-	  }
-    constructor(props){
-
-        super(props);
-        //his.onlogin_demo = this.onlogin_demo.bind(this);
-        this.state = {
-            value:"some arbitary value",
-        };
-        this.state = {
-            data: {},
-            login_in_progress:false
-        };
-
-    }
-
-    // onlogin_demo(data){
-        // this.props.onlogin(data);
-    // }
-
-    render(){
-        const { data } = this.state
-        const { login_in_progress } = this.state
-
-        var login_form =             <form>
-              <div className="form-row align-items-center">
-                <div className="col-auto">
-
-                  <input type="text" className="form-control mb-2" id="email" placeholder="Enter keyword here"  />
-                </div>
-
-
-                <div className="col-auto">
-                  <button onClick={(e) => this.baz(e)} type="submit" className="btn btn-primary mb-2">Find</button>
-
-                </div>
+  }
+  render(){
+    let {page} = this.state;
+    return(
+      <div>
+            <div className="row">
+              <div class="col-lg-2">
+                  <svg class="bd-placeholder-img img-thumbnail" width="200" height="200" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="A generic square placeholder image with a white border around it, making it resemble a photograph taken with an old instant camera: 200x200"><rect width="100%" height="100%" fill="#868e96"></rect><text x="50%" y="50%" fill="#dee2e6" dy=".3em">200x200</text></svg>
               </div>
-            </form>
-        var loader_text = login_in_progress ? "Loading..." : ""
-        return (
-            <div>
-            {login_form}
-            {loader_text}
+              <div class="col-lg-10">
+                <h2>{page.title}</h2>
+                <p>{page.subtitle}</p>
+                <hr />
+                <Breadcrumbs
+                  separator={<b> / </b>}
+                  item={NavLink}
+                  finalItem={"b"}
+                  finalProps={{
+                    style: {}
+                  }}
+                />
+                <BreadcrumbsItem to="/public/:id">Base</BreadcrumbsItem>
+              </div>
             </div>
-        );
-    }
+            <div className="row">
+              <div class="col-lg-12">
+                <hr />
+                <h5>Quick Overview</h5>
+                <p>{page.quick_overview}</p>
+                <hr />
+                <h5>Latest Events</h5>
+                
+                <ul>
+                  {page.lastest_events.map((key, index)=>{return <li><Link to={{ pathname: "/private/service/eduman/"+"brur", search: "?action=page&taskid="+"todo" }}>{key["title"]}</Link></li>})}
+                </ul>
+                <hr />
+                <h5>What are you looking for?</h5>
+                
+                <ul>
+                  {page.sections.map((key, index)=>{return <li><Link to={{ pathname: "/public/:id", search: "?action=page&viewid="+key["comid"] }}>{key["description"]}</Link></li>})}
+                </ul>
+                </div>
+            </div>
+      </div>
+    );
+  }
 }
---
+++ src/institute/page/contact.js
@@ -0,0 +1,21 @@
+import React, {Component, Suspense} from 'react';
+import $ from 'jquery';
+import { BrowserRouter as Router, Route, Link, NavLink } from "react-router-dom";
+import { withRouter} from "react-router-dom";
+import {BreadcrumbsItem } from "react-breadcrumbs-dynamic";
+
+
+
+
+
+class Contacts extends Component{
+    render(){
+        return(
+            <div>
+                TODO
+            </div>
+        );
+    }
+}
+
+export default Contacts;
